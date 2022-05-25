52cab26058f32446341fb0127874674f7cad51af
EC2 Default User <ec2-user@ip-172-31-1-111.ec2.internal>
2022-05-25 14:40:04 +0000

    [update]Bookers2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_22_152700) do
+ActiveRecord::Schema.define(version: 2022_05_25_121155) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,11 @@ ActiveRecord::Schema.define(version: 2022_05_22_152700) do
     t.string "title"
     t.integer "user_id"
     t.text "body"
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "post_images", force: :cascade do |t|
